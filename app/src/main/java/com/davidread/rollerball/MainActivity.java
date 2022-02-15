e1dfc7df12114cf12d2fd467ad9e5b90506e6cba
David Read <49120229+david-read-iii@users.noreply.github.com>
2022-02-15 14:30:40 -0500

    Implement MainActivity.java
    activity_main.xml:
    - Modify layout to display a RollerSurfaceView that takes the whole layout of the parent.
    
    MainActivity.java:
    - Add mSensorManager and mAccelerometer member variables for getting accelerometer values from the device.
    - Add mSurfaceView for displaying the Rollerball game UI.
    - Setup member variables in onCreate().
    - Register accelerometer listener in onResume(). Unregister it in onPause().
    - Implement SensorEventListener interface in this class to pass accelerometer data to mSurfaceView.

--- app/src/main/java/com/davidread/rollerball/MainActivity.java
+++ app/src/main/java/com/davidread/rollerball/MainActivity.java
@@ -2,13 +2,90 @@ package com.davidread.rollerball;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ *
+ */
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
+    /**
+     * {@link SensorManager} for accessing the device's sensors.
+     */
+    private SensorManager mSensorManager;
+
+    /**
+     * {@link Sensor} for accessing the device's accelerometer data.
+     */
+    private Sensor mAccelerometer;
+
+    /**
+     * {@link RollerSurfaceView} to display the UI of the Rollergame.
+     */
+    private RollerSurfaceView mSurfaceView;
+
+    /**
+     * Invoked once when {@link MainActivity} is initially created. It simply initializes member
+     * variables.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        mSurfaceView = findViewById(R.id.rollerSurface);
+        mSurfaceView.setOnClickListener(view -> mSurfaceView.shake());
+    }
+
+    /**
+     * Invoked when {@link MainActivity} enters the foreground. It simply registers the listener
+     * implemented in this activity with {@link #mSensorManager}.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    /**
+     * Invoked when {@link MainActivity} leaves the foreground. It simply unregisters the listener
+     * set in {@link #onResume()}.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this, mAccelerometer);
+    }
+
+    /**
+     * Invoked when a new sensor event occurs. It notifies {@link #mSurfaceView} of the new
+     * accelerometer values.
+     *
+     * @param sensorEvent The {@link SensorEvent} that occurred.
+     */
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+
+        // Get accelerometer values.
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+
+        // Move the ball.
+        mSurfaceView.changeAcceleration(x, y);
+    }
+
+    /**
+     * Invoked when the accuracy of the registered sensor has changed. This does nothing.
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
     }
 }
